e7270cf5d187f450043a45fcc08c7ba547a82215
hiago <silvahiago1599@gmail.com>
2022-07-20 19:24:45 -0300

    feat(app): add loginRoute ao app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { errorMiddleware } = require('./middlewares')
+const { loginRoute } = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/login', loginRoute);
 
 app.use(errorMiddleware);
 
