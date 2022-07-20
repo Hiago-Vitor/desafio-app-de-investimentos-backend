cb62f09bde4417f639e0132bccd8d10215cec77f
hiago <silvahiago1599@gmail.com>
2022-07-20 19:15:45 -0300

    feat: service de login e metdo authClient

---
+++ src/services/index.js
@@ -0,0 +1,3 @@
+const loginService = require('./loginService');
+
+module.exports = { loginService };
---
+++ src/services/loginService.js
@@ -0,0 +1,14 @@
+const { Client } = require('../database/models');
+const { createToken } = require('../utils/jwt');
+
+const authClient = async ({ email, password }) => {
+    const clientData = await Client.findOne({ where: { email, password } });
+    
+    if (!clientData) return false;
+
+  const token = createToken(clientData.dataValues);
+
+  return { token };
+};
+
+module.exports = { authClient };
