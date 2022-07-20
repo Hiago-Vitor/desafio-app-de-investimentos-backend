8bc24f0d78dc091cdebc7dd26ab74a8109c3d608
hiago <silvahiago1599@gmail.com>
2022-07-20 17:01:19 -0300

    feat: add mdwr generico ao app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const { errorMiddleware } = require('./middlewares')
 
 const app = express();
 
 app.use(express.json());
+
+
+app.use(errorMiddleware);
 
 module.exports = app;
