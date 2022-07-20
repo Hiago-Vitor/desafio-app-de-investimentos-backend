0603efced5532aa36a9ee30b04abdbe4f47abba7
hiago <silvahiago1599@gmail.com>
2022-07-20 19:26:17 -0300

    style: formatando e removendo importação

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
-const process = require('process');
 const app = require('./app');
 
 const { API_PORT } = process.env;
 app.listen(API_PORT, () => {
-    console.log(`Escutando a porta: ${API_PORT}`);
-  });
+  console.log(`Escutando a porta: ${API_PORT}`);
+});
