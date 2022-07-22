1c5988cd381f431dfcf0dfab17638afe659e97d1
hiago <silvahiago1599@gmail.com>
2022-07-22 00:05:58 -0300

    feat(migrations): alterando congs das migrations

--- src/database/migrations/20220717154808-create-client.js
+++ src/database/migrations/20220717154808-create-client.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable("Clients", {
       idClient: {
         allowNull: false,
@@ -30,7 +30,7 @@ module.exports = {
     });
   },
 
-  async down(queryInterface, Sequelize) {
+  down: async (queryInterface, _Sequelize) => {
     await queryInterface.dropTable("Clients");
   }
 };
--- src/database/migrations/20220717165752-create-asset.js
+++ src/database/migrations/20220717165752-create-asset.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable("Assets", {
       idAsset: {
         allowNull: false,
@@ -24,7 +24,7 @@ module.exports = {
     });
   },
 
-  async down(queryInterface, Sequelize) {
+  down: async (queryInterface, _Sequelize) => {
     await queryInterface.dropTable("Assets");
   }
 };
