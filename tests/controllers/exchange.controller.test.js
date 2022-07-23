77777e6391593d4ce4f130029cc04a37ff829ebe
hiago <silvahiago1599@gmail.com>
2022-07-23 18:07:21 -0300

    feat(testes):  testando exchangeController

---
+++ tests/controllers/exchange.controller.test.js
@@ -0,0 +1,38 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+const exchangeController = require('../../src/controllers/exchangeController');
+const exchangeService = require('../../src/services/exchangeService');
+
+chai.use(sinonChai);
+const expect = chai.expect;
+
+describe('Test exchangeController', () => {
+    describe('getAllAssets', () => {
+        const response = {};
+        const request = { };
+        const servResponse = [
+            { idAsset: 1, tag: 'RRRP3', quantity: 93, price: 28.74 },
+            { idAsset: 2, tag: 'ALPA4', quantity: 95, price: 17.98 }
+          ];
+
+        before(() => {
+            response.status = sinon.stub().returns(response);
+            response.json = sinon.stub().returns(servResponse);
+            sinon.stub(exchangeService, 'getAll').resolves(servResponse);
+        });
+
+        after(() => {
+            exchangeService.getAll.restore();
+        });
+
+        it('returns with status 200 and with an object', async () => {
+            const res = await exchangeController.getAllAssets(request, response);
+            
+            expect(response.status).to.have.been.calledOnceWith(200);
+            expect(res).to.be.equal(servResponse);
+
+        });
+    });
+
+});
