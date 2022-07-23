4548ccf4d365736e8d7f03933c44a9ecb2d4d43e
hiago <silvahiago1599@gmail.com>
2022-07-23 13:50:07 -0300

    feat(investimentsRoutes): add service sellAsset a uma rota

--- src/routes/investmentsRoute.js
+++ src/routes/investmentsRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { buyAsset } = require('../controllers/investmentController');
+const { buyAsset, sellAsset } = require('../controllers/investmentsController');
 const { validateToken, validateInvestiment } = require('../middlewares');
 
 const routes = express.Router();
 
 routes.post('/buy', validateToken, validateInvestiment, buyAsset);
+
+routes.post('/sell', validateToken, validateInvestiment, sellAsset);
 
 module.exports = routes;
