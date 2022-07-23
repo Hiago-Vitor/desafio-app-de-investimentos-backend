0511fc934cc282ea9b3c6ce1cb3e2469e4180ecd
hiago <silvahiago1599@gmail.com>
2022-07-23 13:40:43 -0300

    style: removendo arquivos

--- src/controllers/investmentController.js
+++
@@ -1,9 +0,0 @@
-const { investimentService: { buyAssetServ } } = require('../services');
-
-const buyAsset = async (req, res) => {
-    const responseData = await buyAssetServ(req.body, req.onSale);
-    
-    return res.status(201).json(responseData);
-};
-
-module.exports = { buyAsset };
