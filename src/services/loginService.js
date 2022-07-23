87a0c53796a9a0800c3efe4e229022aad95aa9b2
hiago <silvahiago1599@gmail.com>
2022-07-23 13:49:03 -0300

    feat(login): modf tratamento de erro

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -2,9 +2,11 @@ const { Client } = require('../database/models');
 const { createToken } = require('../utils/jwt');
 
 const authClient = async ({ email, password }) => {
-    const clientData = await Client.findOne({ where: { email, password } });
-    
-    if (!clientData) return false;
+  const clientData = await Client.findOne({ where: { email, password } });
+
+  if (!clientData) {
+    throw { status: 401, message: 'login não autorizado' };
+  }
 
   const token = createToken(clientData.dataValues);
 
