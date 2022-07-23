ab37502e2a6d7803082a99d4cec99616a835c09f
hiago <silvahiago1599@gmail.com>
2022-07-23 13:50:34 -0300

    feat(app): ligando rotas ao app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 require('express-async-errors');
 const { errorMiddleware } = require('./middlewares');
-const { loginRoute, investimentRoute } = require('./routes');
+const {
+    loginRoute, investimentsRoute, assetsRoute, accountRoute, exchangeRoute } = require('./routes');
 
 const app = express();
 
@@ -9,7 +10,13 @@ app.use(express.json());
 
 app.use('/login', loginRoute);
 
-app.use('/investiments', investimentRoute);
+app.use('/investiments', investimentsRoute);
+
+app.use('/assets', assetsRoute);
+
+app.use('/account', accountRoute);
+
+app.use('/exchange', exchangeRoute);
 
 app.use(errorMiddleware);
 
