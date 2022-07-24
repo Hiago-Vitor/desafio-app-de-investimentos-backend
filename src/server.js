3d5960455146440e8bae13ae62952429e59d73ef
hiago <silvahiago1599@gmail.com>
2022-07-24 10:32:28 -0300

    comitando no heroku

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
 
-const { API_PORT } = process.env;
+const { API_PORT } = process.env || 3000;
 app.listen(API_PORT, () => {
   console.log(`Escutando a porta: ${API_PORT}`);
 });
