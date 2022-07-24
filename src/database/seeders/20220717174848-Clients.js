ab2885f9921050c6493a3b7b024fcee2a21f3a0b
hiago <silvahiago1599@gmail.com>
2022-07-23 21:27:40 -0300

    feat: add bcrypt na autenticação do login

--- src/database/seeders/20220717174848-Clients.js
+++ src/database/seeders/20220717174848-Clients.js
@@ -7,19 +7,19 @@ module.exports = {
         {
           name: 'teste',
           email: 'test@test.com',
-          password: 'password',
+          password: '$2a$05$LEOfuTQg5aJPmy0oBymrie4YPatRMrWnMg7DeG4f4m92dFJjmR8He',
           balance: 1000.49
         },
         {
           name: 'teste-2',
           email: 'other@test.com',
-          password: 'password',
+          password: '$2a$05$rzDq2iWC0ErZJgmzUl96b.ziHOErNRNOkAIwabs1BHPpb8CwngLOG',
           balance: 700.49
         },
       ],
       { timestamps: false });
   },
-
+// $2a$05$FiB8TDHFl9s/0HtC7W6g0ubSi5GYE5cOE7ygBfYcrB5G2OcJ2Ylai
   down: async (queryInterface, _Sequelize) => {
     await queryInterface.bulkDelete('Clients', null, {});
   }
