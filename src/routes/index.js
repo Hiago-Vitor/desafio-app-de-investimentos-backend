f6d25a34774218461f352a74a7a69ea3f901146a
hiago <silvahiago1599@gmail.com>
2022-07-24 21:00:36 -0300

    style: renomeando ou apagando arquivos de rota

--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const loginRoute = require('./loginRoute');
-const investimentsRoute = require('./investmentsRoute');
-const assetsRoute = require('./assetsRoutes');
-const accountRoute = require('./accountRoute');
-const exchangeRoute = require('./exchangeRoute');
-
-module.exports = { loginRoute, investimentsRoute, assetsRoute, accountRoute, exchangeRoute };
